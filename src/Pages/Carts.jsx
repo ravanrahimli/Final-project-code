3a8ce31e05f698639a53b8678a692c94f95615f9
ravanrahimli <rahimliravan7@gmail.com>
2024-12-17 15:44:25 +0400

    Footer change

---
+++ src/Pages/Carts.jsx
@@ -0,0 +1,15 @@
+import Cart from "../Components/Cart"
+import Navbar from "../Components/Navbar"
+import Footer from "../Components/Footer"
+
+function Carts() {
+  return (
+    <div>
+        <Navbar/>
+        <Cart/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Carts
